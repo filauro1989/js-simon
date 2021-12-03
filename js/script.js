12b370edcfcf99dfe175b02fb136b286102642c8
filauro1989 <filauro1989@gmail.com>
2021-12-03 14:51:44 +0100

    prompt

--- js/script.js
+++ js/script.js
@@ -1,10 +1,6 @@
-
-// Dopo 30 secondi l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
-
-// Visualizzare in pagina 5 numeri casuali. 
-
+// Visualizzare in pagina 5 numeri casuali poi fateli sparire.
 let randomNumbersContainer = document.querySelector('.numbers-container');
 let randNumbers = [];
 
@@ -22,7 +18,7 @@ for (let index = 0; index < 5; index++) {
     console.log('pippo', generatedNumber);
     
     while (randNumbers.includes(generatedNumber)) {
-        generatedNumber = getRandomIntInclusive(1, 5);
+        generatedNumber = getRandomIntInclusive(1, 20);
     };
         
     randNumbers.push(generatedNumber);
@@ -31,12 +27,29 @@ console.log(randNumbers);
 
 randomNumbersContainer.append(randNumbers);
 
-// Da lì parte un timer di 30 secondi.
-// Dopo 30 secondi l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+// function stopTimer2 () {
+//     clearTimeout(timer2)
+// }
 
-let timer = setTimeout(function(){
+// Da lì parte un timer di 30 secondi
+let timer1 = setTimeout(function(){
     randomNumbersContainer.innerHTML = '';
 }, 2000);
+
+
+// Dopo 30 secondi l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt()
+
+    let timer2 = setTimeout(function(){
+
+        let userNumbers = [];
+        for (let index = 0; index < 5; index++) {
+            userNumbers.push(parseInt(prompt('inserisci numero')))
+            console.log('userNumbers', userNumbers);
+        }
+    }, 2200);
+
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
 
 
 
